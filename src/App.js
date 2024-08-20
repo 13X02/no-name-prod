7d2784762e6acad3add146cfd7abe9864bff0b08
Abhijith <abhijithh496@gmail.com>
2024-08-20 20:37:51 +0530

    +ui

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Domain from './components/Domains/Domain';
 import Landing from './components/Landing/Landing';
 import Benefits from './components/Benefits/Benefits';
+import Strength from './components/Strengths/Strength';
 
 
 function App() {
@@ -14,17 +15,35 @@ function App() {
       <Landing />
       <Domain />
       <Benefits/>
-      <div className='px-20 py-24'>
-        <div className='w-[572px] h-[459px] p-10 bg-[#F4F4F4] flex flex-col gap-5 '>
-          <p>
-          Innovating for the Future of Business"
-          </p>
-          <p>
-          At Cyan Cloud Technologies, innovation is at the core of everything we do. Our products are designed to harness the latest advancements in AI, automation, and cloud technologies to drive your business forward.We’re committed to pushing the boundaries of technology, ensuring that your company is always equipped with the tools of tomorrow.
-          </p>
-          <button>View More</button>
+      <Strength/>
+      <div className='flex flex-col bg-[#E8F4FA] px-96 py-52 justify-center items-center font-poppins gap-5'>
+        <p className='text-3xl font-semibold'>Start using the best in class softwares
+        </p>
+        <div className='flex gap-5'>
+          <input className='h-16 w-[530px] rounded-lg px-10 shadow-md'placeholder='Enter your email'/>
+          <button className='w-44 h-16 bg-black text-white rounded-lg'>Subscribe</button>
         </div>
       </div>
+      <footer>
+        <div>
+          <div>
+
+            <p>CYAN</p>
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </footer>
     </>
   );
 }
---
+++ src/components/Strengths/Strength.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Strength = () => {
+  return (
+    <div>
+              <div className='px-20 py-24 font-poppins flex gap-24'>
+        
+        <div className='rounded-xl p-10 bg-[#F4F4F4]  flex flex-col gap-5 items-center justify-center text-center '>
+          <p className='text-2xl font-semibold'>
+          Innovating for the Future of Business
+          </p>
+          <p className='text-base text-[#545454]'>
+          At Cyan Cloud Technologies, innovation is at the core of everything we do. Our products are designed to harness the latest advancements in AI, automation, and cloud technologies to drive your business forward.We’re committed to pushing the boundaries of technology, ensuring that your company is always equipped with the tools of tomorrow.
+          </p>
+          <button className='h-12 w-40 bg-[#1090CB] text-white rounded-lg'>View More</button>
+        </div>
+        <div className='rounded-xl p-10 bg-[#1090CB] text-white flex flex-col gap-5 items-center justify-center text-center '>
+          <p className='text-2xl font-semibold'>
+          Innovating for the Future of Business
+          </p>
+          <p className='text-base text-white'>
+          At Cyan Cloud Technologies, innovation is at the core of everything we do. Our products are designed to harness the latest advancements in AI, automation, and cloud technologies to drive your business forward.We’re committed to pushing the boundaries of technology, ensuring that your company is always equipped with the tools of tomorrow.
+          </p>
+          <button className='h-12 w-40 bg-white text-[#1090CB] rounded-lg'>View More</button>
+        </div>
+        
+      </div>
+      <div className='flex px-20 py-24 font-poppins flex-col gap-10 text-2xl justify-center items-center '>
+          <p> You will be in good Company </p>
+          <div className='flex gap-16 justify-center items-center w-full'>
+            <div className='w-[185px] h-[185px] bg-gray-400'>
+
+            </div>
+            <div className='w-[185px] h-[185px] bg-gray-400'>
+
+            </div>
+            <div className='w-[185px] h-[185px] bg-gray-400'>
+
+            </div>
+            <div className='w-[185px] h-[185px] bg-gray-400'>
+
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Strength
